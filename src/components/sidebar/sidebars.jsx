4b05d2215e4c74390d93a14e3b323aa90df706bf
raihan2-byte <raihanalfarisi4@gmail.com>
2024-06-03 14:09:26 +0700

    fix sidebar

--- src/components/sidebar/sidebars.jsx
+++ src/components/sidebar/sidebars.jsx
@@ -6,7 +6,7 @@ import LogoAtmajaya from "../../assets/logo.png"
 
 const Sidebars = () => {
   return (
-    <div className="w-[300px] bg-[#FFDAB4] text-[#000] h-full overflow-auto block text-[12px]">
+    <div className="w-[300px] bg-[#FFDAB4] text-[#000] h-full overflow-auto block text-[12px] font-bold">
       <div>
           <img src={LogoAtmajaya} alt="" className="w-[230px] m-auto p-1"/>
         </div>
