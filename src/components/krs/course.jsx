fde371688e3f106d53b73052dd6d45ae08ec94c5
raihan2-byte <raihanalfarisi4@gmail.com>
2024-06-03 14:07:02 +0700

    fix course with different semester

--- src/components/krs/course.jsx
+++ src/components/krs/course.jsx
@@ -2,56 +2,13 @@ import React, { useState } from "react";
 import ExpandKrs from "../../assets/expand-krs.png";
 import ExpandHideDetails from "../../assets/expand-hidedetails.png";
 
-const CourseSem1 = [
-  {
-    CourseCode: "TIN 101",
-    Description: "MATEMATIKA DASAR",
-    Units: "2",
-    When: "ODD",
-    Day: "Mon 10.00-12.00 WIB",
-    Room: "Dormitory 1 BSD",
-    Instructor: "Trifenaus Prabu Hidayat, S.T., M.T.",
-    Grade: "",
-  },
-  {
-    CourseCode: "TIN 102",
-    Description: "Bahasa Indonesia",
-    Units: "2",
-    When: "ODD",
-    Day: "Tues 10.00-12.00 WIB",
-    Room: "AG 101 BSD",
-    Instructor: "Andre Sugioko, S.T.,M.T.",
-    Grade: "",
-  },
-  {
-    CourseCode: "TIN 103",
-    Description: "Bahasa Inggris",
-    Units: "2",
-    When: "ODD",
-    Day: "Wed 10.00-12.00 WIB",
-    Room: "A 1102 BSD",
-    Instructor: "Hotma Antoni Hutahaean, S.T., M.T., IPM",
-    Grade: "",
-  },
-  {
-    CourseCode: "TIN 104",
-    Description: "Kimia Dasar",
-    Units: "2",
-    When: "ODD",
-    Day: "Fri 10.00-12.00 WIB",
-    Room: "Dormitory 3 SMG",
-    Instructor: "Ronald Sukwadi, Ph.D., IPM",
-    Grade: "",
-  },
-];
-
-const course = () => {
-  const [CoreCoursesSem1, setCoreCoursesSem1] = useState(false);
+const Course = ({ courses, semester }) => {
+  const [showCourses, setShowCourses] = useState(false);
 
   return (
     <div className="text-[12px]">
       <div className="flex bg-[#D9D9D9] justify-between text-[12px] text-[#000000] font-bold">
-        <div className="px-8">INDUSTRIAL ENGINEERING - SEMESTER 1</div>
+        <div className="px-8">INDUSTRIAL ENGINEERING - SEMESTER {semester}</div>
       </div>
       <div className="dropdown w-full">
         <div className="flex justify-between items-center pl-8 pr-2 ">
@@ -59,7 +16,7 @@ const course = () => {
             <button
               className="text-black font-semibold py-1 items-center "
               onClick={() => {
-                setCoreCoursesSem1(!CoreCoursesSem1);
+                setShowCourses(!showCourses);
               }}
             >
               <div>
@@ -77,38 +34,26 @@ const course = () => {
         </div>
 
         <ul
-          className={`dropdown-menu  ${
-            CoreCoursesSem1 ? "block" : "hidden"
-          } bg-white text-black-700 `}
+          className={`dropdown-menu ${showCourses ? "block" : "hidden"} bg-white text-black-700 `}
         >
           <li className="flex justify-between items-center w-full"></li>
           <div className="px-8 my-2">
             <table className="border-[#888888] w-[90%] ">
               <thead className="border-[1px] border-black">
                 <tr className="text-left font-bold ">
-                  <th className="border-[1px] border-[#888888] pl-2">
-                    Course Code
-                  </th>
-                  <th className="border-[1px] border-[#888888] pl-2">
-                    Description
-                  </th>
+                  <th className="border-[1px] border-[#888888] pl-2">Course Code</th>
+                  <th className="border-[1px] border-[#888888] pl-2">Description</th>
                   <th className="border-[1px] border-[#888888] pl-2">Units</th>
                   <th className="border-[1px] border-[#888888] pl-2">When</th>
-                  <th className="border-[1px] border-[#888888] pl-2 text-center">
-                    Day
-                  </th>
-                  <th className="border-[1px] border-[#888888] pl-2 text-center">
-                    Room
-                  </th>
-                  <th className="border-[1px] border-[#888888] pl-2 text-center">
-                    Instructor
-                  </th>
+                  <th className="border-[1px] border-[#888888] pl-2 text-center">Day</th>
+                  <th className="border-[1px] border-[#888888] pl-2 text-center">Room</th>
+                  <th className="border-[1px] border-[#888888] pl-2 text-center">Instructor</th>
                   <th className="border-[1px] border-[#888888] pl-2">Grade</th>
                 </tr>
               </thead>
               <tbody className="border-[1px] border-[#888888] text-[12px]">
-                {CourseSem1.map((item, index) => (
-                  <tr className="border-[1px] border-[#888888]">
+                {courses.map((item, index) => (
+                  <tr key={index} className="border-[1px] border-[#888888]">
                     <td className="border-[1px] border-[#888888] pl-2">
                       <div className="flex gap-2 items-center">
                         <div>
@@ -116,33 +61,19 @@ const course = () => {
                             id="default-checkbox"
                             type="checkbox"
                             value=""
-                            className="w-4 h-4 flex items-center text-blue-600 bg-gray-100 border-gray-300 rounded  dark:bg-gray-700 dark:border-gray-600"
+                            className="w-4 h-4 flex items-center text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
                           />
                         </div>
                         <div>{item.CourseCode}</div>
                       </div>
                     </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Description}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Units}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.When}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Day}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Room}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Instructor}
-                    </td>
-                    <td className="border-[1px] border-[#888888] pl-2">
-                      {item.Grade}
-                    </td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Description}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Units}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.When}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Day}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Room}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Instructor}</td>
+                    <td className="border-[1px] border-[#888888] pl-2">{item.Grade}</td>
                   </tr>
                 ))}
               </tbody>
@@ -154,4 +85,4 @@ const course = () => {
   );
 };
 
-export default course;
+export default Course;
--- src/components/krs/krs.jsx
+++ src/components/krs/krs.jsx
@@ -5,6 +5,179 @@ import { IoIosAdd } from "react-icons/io";
 
 import AddIcon from "../../assets/add.png"
 
+const CourseSem1 = [
+  {
+    CourseCode: "TIN 101",
+    Description: "MATEMATIKA DASAR",
+    Units: "2",
+    When: "ODD",
+    Day: "Mon 10.00-12.00 WIB",
+    Room: "Dormitory 1 BSD",
+    Instructor: "Trifenaus Prabu Hidayat, S.T., M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 102",
+    Description: "Bahasa Indonesia",
+    Units: "2",
+    When: "ODD",
+    Day: "Tues 10.00-12.00 WIB",
+    Room: "AG 101 BSD",
+    Instructor: "Andre Sugioko, S.T.,M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 103",
+    Description: "Bahasa Inggris",
+    Units: "2",
+    When: "ODD",
+    Day: "Wed 10.00-12.00 WIB",
+    Room: "A 1102 BSD",
+    Instructor: "Hotma Antoni Hutahaean, S.T., M.T., IPM",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 104",
+    Description: "Kimia Dasar",
+    Units: "2",
+    When: "ODD",
+    Day: "Fri 10.00-12.00 WIB",
+    Room: "Dormitory 3 SMG",
+    Instructor: "Ronald Sukwadi, Ph.D., IPM",
+    Grade: "",
+  },
+];
+
+const CourseSem2 = [
+  {
+    CourseCode: "TIN 201",
+    Description: "Kalkulus I",
+    Units: "2",
+    When: "EVEN",
+    Day: "Mon 10.00-12.00 WIB",
+    Room: "Dormitory 1 BSD",
+    Instructor: "Trifenaus Prabu Hidayat, S.T., M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 202",
+    Description: "Kalkulus II",
+    Units: "2",
+    When: "EVEN",
+    Day: "Tues 10.00-12.00 WIB",
+    Room: "AG 101 BSD",
+    Instructor: "Andre Sugioko, S.T.,M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 203",
+    Description: "Praktikum CAD",
+    Units: "2",
+    When: "EVEN",
+    Day: "Wed 10.00-12.00 WIB",
+    Room: "A 1102 BSD",
+    Instructor: "Hotma Antoni Hutahaean, S.T., M.T., IPM",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 204",
+    Description: "Praktikum Fisika",
+    Units: "2",
+    When: "EVEN",
+    Day: "Fri 10.00-12.00 WIB",
+    Room: "Dormitory 3 SMG",
+    Instructor: "Ronald Sukwadi, Ph.D., IPM",
+    Grade: "",
+  },
+];
+
+const CourseSem3 = [
+  {
+    CourseCode: "TIN 301",
+    Description: "Aljabar Linier",
+    Units: "2",
+    When: "ODD",
+    Day: "Mon 10.00-12.00 WIB",
+    Room: "Dormitory 1 BSD",
+    Instructor: "Trifenaus Prabu Hidayat, S.T., M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 302",
+    Description: "Kewirausahaan",
+    Units: "2",
+    When: "ODD",
+    Day: "Tues 10.00-12.00 WIB",
+    Room: "AG 101 BSD",
+    Instructor: "Andre Sugioko, S.T.,M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 303",
+    Description: "Kewarganegaraan",
+    Units: "2",
+    When: "ODD",
+    Day: "Wed 10.00-12.00 WIB",
+    Room: "A 1102 BSD",
+    Instructor: "Hotma Antoni Hutahaean, S.T., M.T., IPM",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 304",
+    Description: "Agama",
+    Units: "2",
+    When: "ODD",
+    Day: "Fri 10.00-12.00 WIB",
+    Room: "Dormitory 3 SMG",
+    Instructor: "Ronald Sukwadi, Ph.D., IPM",
+    Grade: "",
+  },
+];
+
+const CourseSem4 = [
+  {
+    CourseCode: "TIN 401",
+    Description: "Kerja Praktik/KKN",
+    Units: "2",
+    When: "EVEN",
+    Day: "Mon 10.00-12.00 WIB",
+    Room: "Dormitory 1 BSD",
+    Instructor: "Trifenaus Prabu Hidayat, S.T., M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 402",
+    Description: "Seminar",
+    Units: "2",
+    When: "EVEN",
+    Day: "Tues 10.00-12.00 WIB",
+    Room: "AG 101 BSD",
+    Instructor: "Andre Sugioko, S.T.,M.T.",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 403",
+    Description: "Proses Manufaktur",
+    Units: "2",
+    When: "EVEN",
+    Day: "Wed 10.00-12.00 WIB",
+    Room: "A 1102 BSD",
+    Instructor: "Hotma Antoni Hutahaean, S.T., M.T., IPM",
+    Grade: "",
+  },
+  {
+    CourseCode: "TIN 404",
+    Description: "Tugas Akhir",
+    Units: "2",
+    When: "EVEN",
+    Day: "Fri 10.00-12.00 WIB",
+    Room: "Dormitory 3 SMG",
+    Instructor: "Ronald Sukwadi, Ph.D., IPM",
+    Grade: "",
+  },
+];
+
+
 const krs = () => {
   const [OpenAllCourses, setOpenAllCourses] = useState(false);
 
@@ -12,9 +185,9 @@ const krs = () => {
     <div className="w-full">
       <div className="dropdown w-full">
         <div className="flex py-2 pl-4 bg-[#D9D9D9] justify-between text-[12px] text-[#000000] font-bold">
-          <div className="flex items-center gap-3 ">
+          <div className="flex items-center gap-3">
             <button
-              className="text-black font-semibold py-1 items-center "
+              className="text-black font-semibold py-1 items-center"
               onClick={() => {
                 setOpenAllCourses(!OpenAllCourses);
               }}
@@ -23,26 +196,22 @@ const krs = () => {
                 <img src={ExpandCourses} alt="" />
               </div>
             </button>
-            <div className="">S1 - INDUSTRIAL ENGINEERING</div>
+            <div>S1 - INDUSTRIAL ENGINEERING</div>
           </div>
           <div className="px-2">Not Satisfied</div>
         </div>
-        <ul
-          className={`dropdown-menu  ${
-            OpenAllCourses ? "block" : "hidden"
-          } bg-white text-black-700 `}
-        >
+        <ul className={`dropdown-menu ${OpenAllCourses ? "block" : "hidden"} bg-white text-black-700`}>
           <li className="flex justify-between items-center w-full">
             <div className="flex items-center px-10 py-2 justify-between w-full">
-              <div className="flex items-center   ">
+              <div className="flex items-center">
                 <input
                   id="default-checkbox"
                   type="checkbox"
                   value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded  dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
-                  for="default-checkbox"
+                  htmlFor="default-checkbox"
                   className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   Select All
@@ -58,17 +227,17 @@ const krs = () => {
               </div>
             </div>
           </li>
-          <li className=" justify-between items-center w-full">
-            <Course />
+          <li className="justify-between items-center w-full">
+            <Course courses={CourseSem1} semester={1} />
           </li>
-          <li className=" justify-between items-center w-full">
-            <Course />
+          <li className="justify-between items-center w-full">
+            <Course courses={CourseSem2} semester={2} />
           </li>
-          <li className=" justify-between items-center w-full">
-            <Course />
+          <li className="justify-between items-center w-full">
+            <Course courses={CourseSem3} semester={3} />
           </li>
-          <li className=" justify-between items-center w-full">
-            <Course />
+          <li className="justify-between items-center w-full">
+            <Course courses={CourseSem4} semester={4} />
           </li>
         </ul>
       </div>
